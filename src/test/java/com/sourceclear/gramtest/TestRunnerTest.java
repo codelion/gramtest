95d8d0339aba0d2bbd0a92f3ad1b3820b1750519
Asankhaya Sharma <asankhaya@yahoo.com>
2017-01-05 16:27:20 +0800

    Another library

--- src/test/java/com/sourceclear/gramtest/TestRunnerTest.java
+++ src/test/java/com/sourceclear/gramtest/TestRunnerTest.java
@@ -2,9 +2,13 @@ package com.sourceclear.gramtest;
 
 import java.io.IOException;
 
-import com.eclipsesource.json.Json;
-import com.eclipsesource.json.JsonValue;
-import com.eclipsesource.json.ParseException;
+//import com.eclipsesource.json.Json;
+//import com.eclipsesource.json.JsonValue;
+//import com.eclipsesource.json.ParseException;
+import com.grack.nanojson.JsonObject;
+import com.grack.nanojson.JsonParser;
+import com.grack.nanojson.JsonParserException;
+import com.grack.nanojson.JsonWriter;
 import org.junit.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.SynchronousQueue;
@@ -15,7 +19,7 @@ import java.util.concurrent.SynchronousQueue;
 public class TestRunnerTest {
 
   /**
-   * Test with url grammar
+   * Test with a target library
    * @throws java.io.IOException
    */
   @Test
@@ -30,12 +34,15 @@ public class TestRunnerTest {
   private void consumeTests(BlockingQueue queue) throws InterruptedException {
     while(true) {
       String testCase = ((String) queue.take()).replace("$$$", "\"");
-      JsonValue j;
+      //JsonValue j;
       try {
-        j = Json.parse(testCase);
-        if(!testCase.equals(j.toString())) System.out.println("Inverse Error : " + testCase);
+        //j = Json.parse(testCase);
+        JsonParser.any().from(testCase);
+        //if(!testCase.equals(j.toString()))
+        //  System.out.println("Inverse Error : " + testCase);
       }
-      catch (ParseException e) {
+      //catch (ParseException e) {
+      catch (JsonParserException e) {
         System.out.println("Parse Error : " + testCase);
       }
     }
