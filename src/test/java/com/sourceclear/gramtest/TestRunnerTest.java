a18f095bb33522a69dfc4eccebb56e3f311d6659
Asankhaya Sharma <asankhaya@yahoo.com>
2017-01-05 15:13:43 +0800

    initial imp and run

--- src/test/java/com/sourceclear/gramtest/TestRunnerTest.java
+++ src/test/java/com/sourceclear/gramtest/TestRunnerTest.java
@@ -2,6 +2,9 @@ package com.sourceclear.gramtest;
 
 import java.io.IOException;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonValue;
+import com.eclipsesource.json.ParseException;
 import org.junit.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.SynchronousQueue;
@@ -19,15 +22,22 @@ public class TestRunnerTest {
   @Ignore("Non terminating test case")
   public void testQueueGenerator() throws IOException, InterruptedException {
     final BlockingQueue<String> queue = new SynchronousQueue<>();
-    TestRunner continuousRunner = new TestRunner(getClass().getResourceAsStream("/url.bnf"), queue, 10, 8, 16);
+    TestRunner continuousRunner = new TestRunner(getClass().getResourceAsStream("/json.bnf"), queue, 8, 4, 32);
     new Thread(continuousRunner).start();
     consumeTests(queue);
   }
 
   private void consumeTests(BlockingQueue queue) throws InterruptedException {
     while(true) {
-      String testCase = (String) queue.take();
-      System.out.println(testCase);
+      String testCase = ((String) queue.take()).replace("$$$", "\"");
+      JsonValue j;
+      try {
+        j = Json.parse(testCase);
+        if(!testCase.equals(j.toString())) System.out.println("Inverse Error : " + testCase);
+      }
+      catch (ParseException e) {
+        System.out.println("Parse Error : " + testCase);
+      }
     }
   }
 
