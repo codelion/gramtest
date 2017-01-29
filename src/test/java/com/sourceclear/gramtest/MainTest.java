68afa048bbb72ab58345f82318b91cf69b7a0799
Asankhaya Sharma <asankhaya@yahoo.com>
2017-01-29 12:21:24 +0800

    override transitive dependency to eliminate warning

--- src/test/java/com/sourceclear/gramtest/MainTest.java
+++ src/test/java/com/sourceclear/gramtest/MainTest.java
@@ -136,7 +136,7 @@ public class MainTest {
    * @throws java.io.IOException
    */
   @Test
-  @Ignore("Long running test")
+  @Ignore("Ignoring long running test")
   public void testURLGram() throws IOException {
     Lexer lexer = new bnfLexer(new ANTLRInputStream(getClass().getResourceAsStream("/url.bnf")));
     CommonTokenStream tokens = new CommonTokenStream(lexer);
